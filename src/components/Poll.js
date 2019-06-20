3081287663951df7b9d2bfcc7887a56ca01045f3
Jessica Grzanna <jgrzanna@me.com>
2019-06-20 10:53:08 +0200

    feat: Add Poll component
    - Receives authedUser, users and questions from store
    - Displays name and avatar of user who asked the question
    - Renders a child component depending on which poll card is being requested that either includes the Poll Summary, Poll Question (for unanswered) or Poll Results

---
+++ src/components/Poll.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PollSummary from "./PollSummary";
+import PollQuestion from "./PollQuestion";
+import PollResult from "./PollResult";
+import { Row, Col, Figure } from "react-bootstrap";
+
+class Poll extends Component {
+  render() {
+    const { question } =
+      this.props.location.state === undefined
+        ? this.props
+        : this.props.location.state;
+    const { authedUser, users, location, questions } = this.props;
+    const asked_by = users[question.asked_by].name;
+    const { text_1, text_2, id } = question;
+    return (
+      <div className="poll-component">
+        {console.log("poll props: ", this.props.questions)}
+        <Row>
+          <Col sm={4}>Asked by:</Col>
+          <Col sm={8}>
+            <h4>Would You Rather</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={4}>
+            <Figure>
+              <Figure.Image
+                src={users[question.asked_by].avatarUrl}
+                width={70}
+                height={70}
+                rounded
+              />
+              <Figure.Caption>{asked_by}</Figure.Caption>
+            </Figure>
+          </Col>
+          <Col sm={8}>
+            {location.pathname === "/" ? (
+              <PollSummary
+                text_1={text_1}
+                text_2={text_2}
+                id={id}
+                question={question}
+                authedUser={authedUser}
+                location={location}
+              />
+            ) : questions[id].answered_by.includes(authedUser) === false ? (
+              <PollQuestion question={question} />
+            ) : (
+              <PollResult
+                question={question}
+                authedUser={authedUser}
+                users={users}
+              />
+            )}
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    authedUser: state.authedUser.user_id,
+    users: state.users,
+    questions: state.questions
+  };
+}
+
+export default connect(mapStateToProps)(Poll);
