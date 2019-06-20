d6eb89c04c3099eaac3dcd26abb7a814b7ac9827
Jessica Grzanna <jgrzanna@me.com>
2019-06-20 10:27:27 +0200

    feat: Add Home component
    - The home view displays answered and unanswered questions of authedUser
    - Receives authedUser, users and questions objects from the store

---
+++ src/components/Home.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Container, Tab, Row, Col, Nav } from "react-bootstrap";
+import Poll from "./Poll";
+
+class Home extends Component {
+  render() {
+    const { questions, authedUser, location } = this.props;
+    return (
+      <Container className="home-component">
+        <h1>HOME</h1>
+        <Container>
+          <Tab.Container defaultActiveKey="first">
+            <Row>
+              <Col sm={3}>
+                <Nav variant="pills" className="flex-column">
+                  <Nav.Item>
+                    <Nav.Link eventKey="first">Unanswered Polls</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="second">Answered Polls</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Col>
+              <Col sm={9}>
+                <Tab.Content>
+                  <Tab.Pane eventKey="first">
+                    {Object.values(questions).map(
+                      question =>
+                        question.answered_by.includes(authedUser) === false && (
+                          <Container key={question.id}>
+                            <div className="card card-body">
+                              <Poll
+                                question={question}
+                                authedUser={authedUser}
+                                location={location}
+                              />
+                            </div>
+                          </Container>
+                        )
+                    )}
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="second">
+                    {Object.values(questions).map(
+                      question =>
+                        question.answered_by.includes(authedUser) && (
+                          <div key={question.id} className="card card-body">
+                            <Poll
+                              question={question}
+                              authedUser={authedUser}
+                              location={location}
+                            />
+                          </div>
+                        )
+                    )}
+                  </Tab.Pane>
+                </Tab.Content>
+              </Col>
+            </Row>
+          </Tab.Container>
+        </Container>
+      </Container>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    authedUser: state.authedUser.user_id,
+    users: state.users,
+    questions: state.questions
+  };
+}
+
+export default connect(mapStateToProps)(Home);
