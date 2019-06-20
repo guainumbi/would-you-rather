7010e9f79830de7d4f308dd5d10a543218d8831b
Jessica Grzanna <jgrzanna@me.com>
2019-06-20 10:47:24 +0200

    feat: Add NewQuestion component
    - Controlled component that lets the user create a new question by submitting a form with two input fields for text_1 and text_2
    - Manages the form data in it's own react-state and has a handleSubmit function to dispatch the action handler for adding a new question
    - Redirects to root after form submission

---
+++ src/components/NewQuestion.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { handleAddQuestion } from "../actions/questions";
+import { Container, Button, Form } from "react-bootstrap";
+
+class NewQuestion extends Component {
+  state = {
+    text_1: "",
+    text_2: ""
+  };
+
+  handleChange = e => {
+    const text = e.target.value;
+
+    if (e.target.name === "text_1") {
+      this.setState(() => ({
+        text_1: text
+      }));
+    } else {
+      this.setState(() => ({
+        text_2: text
+      }));
+    }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { text_1, text_2 } = this.state;
+    const { dispatch, history } = this.props;
+
+    dispatch(handleAddQuestion(text_1, text_2));
+
+    this.setState(() => ({
+      text_1: "",
+      text_2: ""
+    }));
+
+    history.push("/");
+  };
+  render() {
+    const { text_1, text_2 } = this.state;
+    return (
+      <Container>
+        <h1>NEW QUESTION</h1>
+        <Container>Would you rather</Container>
+        <Form className="new-form" onSubmit={this.handleSubmit}>
+          <Form.Control
+            as="textarea"
+            name="text_1"
+            placeholder="Enter Option 1"
+            value={text_1}
+            onChange={this.handleChange}
+            className="textarea"
+          />
+          <Container>OR</Container>
+          <Form.Control
+            as="textarea"
+            name="text_2"
+            placeholder="Enter Option 2"
+            value={text_2}
+            onChange={this.handleChange}
+            className="textarea"
+          />
+          <div>
+            <Button type="submit" disabled={text_1 === "" || text_2 === ""}>
+              Submit
+            </Button>
+          </div>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+export default withRouter(connect()(NewQuestion));
