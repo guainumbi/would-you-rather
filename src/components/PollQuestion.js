4a8a6f35704ec2b48a28a0035715780ad0a945c2
Jessica Grzanna <jgrzanna@me.com>
2019-06-20 11:24:47 +0200

    feat: Add PollQuestion component
    - Controlled component with two radio options that sends the users answer on submit via dispatching handleAnswerSubmit
    - Receives questions from redux store via mapStateToProps

---
+++ src/components/PollQuestion.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Container, Button, Form } from "react-bootstrap";
+import { handleAnswerSubmit } from "../actions/shared";
+
+class PollQuestion extends Component {
+  state = {
+    answer: null
+  };
+
+  handleChange = e => {
+    const choice = e.target.value;
+    this.setState(() => ({
+      answer: choice
+    }));
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { dispatch, question } = this.props;
+    dispatch(handleAnswerSubmit(this.state.answer, question.id));
+  };
+
+  render() {
+    const { question } = this.props;
+    const { text_1, text_2 } = question;
+    return (
+      <div className="poll-details">
+        {console.log("question props_ ", this.props)}
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Check
+            type="radio"
+            name="choiceOption"
+            label={text_1}
+            value={text_1}
+            onChange={this.handleChange}
+          />
+          <b>OR</b>
+          <Form.Check
+            type="radio"
+            name="choiceOption"
+            label={text_2}
+            value={text_2}
+            onChange={this.handleChange}
+          />
+          <Container>
+            <Button type="submit">Submit</Button>
+          </Container>
+        </Form>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    questions: state.questions
+  };
+}
+
+export default connect(mapStateToProps)(PollQuestion);
